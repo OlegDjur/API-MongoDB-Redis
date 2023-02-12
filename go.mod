18147a1688631a0001c86903fc66619435c08b0b
oleg <olegdjur@gmail.com>
2023-02-12 19:55:56 +0600

    created mongo and redis connection

--- go.mod
+++ go.mod
@@ -3,16 +3,20 @@ module github.com/OlegDjur/API-MongoDB-Redis
 go 1.19
 
 require (
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/gin-gonic/gin v1.8.2
+	github.com/redis/go-redis/v9 v9.0.2
+	github.com/spf13/viper v1.15.0
+	go.mongodb.org/mongo-driver v1.11.1
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.2 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
-	github.com/go-redis/redis v6.15.9+incompatible
-	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -31,14 +35,12 @@ require (
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.15.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.11.1 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/OlegDjur/API-MongoDB-Redis/configs"
 	"github.com/gin-gonic/gin"
-	"github.com/go-redis/redis"
+	"github.com/redis/go-redis/v9"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	_ "go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -17,8 +19,8 @@ import (
 var (
 	server      *gin.Engine
 	ctx         context.Context
-	mongoClient mongo.Client
-	redisClient redis.Client
+	mongoClient *mongo.Client
+	redisClient *redis.Client
 )
 
 func init() {
@@ -43,15 +45,15 @@ func init() {
 	fmt.Println("MongoDB successfully connected...")
 
 	// Connect to Redis
-	redisClient := redis.NewClient(&redis.Options{
+	redisClient = redis.NewClient(&redis.Options{
 		Addr: config.RedisUri,
 	})
 
-	if _, err := redisClient.Ping().Result(); err != nil {
+	if _, err := redisClient.Ping(ctx).Result(); err != nil {
 		panic(err)
 	}
 
-	err = redisClient.Set("test", "Welcome to Golang with Redis and MongoDB", 0).Err()
+	err = redisClient.Set(ctx, "test", "Welcome to Golang with Redis and MongoDB", 0).Err()
 	if err != nil {
 		panic(err)
 	}
@@ -59,4 +61,29 @@ func init() {
 	fmt.Println("Redis client connected successfully...")
 
 	server = gin.Default()
+}
+
+func main() {
+	config, err := configs.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Could not load config", err)
+	}
+
+	defer mongoClient.Disconnect(ctx)
+
+	fmt.Println(redisClient)
+	value, err := redisClient.Get(ctx, "test").Result()
+
+	if err == redis.Nil {
+		fmt.Println("key: test does not exist")
+	} else if err != nil {
+		panic(err)
+	}
+
+	router := server.Group("/api")
+	router.GET("/healthchecker", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": value})
+	})
+
+	log.Fatal(server.Run(":" + config.Port))
 }
