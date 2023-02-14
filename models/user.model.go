ce7be74f19cd9003b496c3f0db0e922c07198d07
oleg <olegdjur@gmail.com>
2023-02-14 18:26:43 +0600

    created user models

---
+++ models/user.model.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type SignUpInput struct {
+	Name            string    `json:"name" bson:"name" binding:"required"`
+	Email           string    `json:"email" bson:"email" binding:"required"`
+	Password        string    `json:"password" bson:"password" binding:"required,min=8"`
+	PasswordConfirm string    `json:"passwordConfirm" bson:"passwordConfirm,omitempty" binding:"reuired"`
+	Role            string    `json:"role" bson:"role"`
+	Verifed         bool      `json:"verifed" bson:"verifed"`
+	CreatedAt       time.Time `json:"creted_at" bson:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at" bson:"updated_at"`
+}
+
+type SignInInput struct {
+	Email    string `json:"email" bson:"email" binding:"required"`
+	Password string `json:"password" bson:"password" binding:"required"`
+}
+
+type DBResponse struct {
+	ID              primitive.ObjectID `json:"id" bson:"_id"`
+	Name            string             `json:"name" bson:"name"`
+	Email           string             `json:"email" bson:"email"`
+	Password        string             `json:"password" bson:"password"`
+	PasswordConfirm string             `json:"passwordConfirm,omitempty" bson:"passwordConfirm,omitempty"`
+	Role            string             `json:"role" bson:"role"`
+	Verified        bool               `json:"verified" bson:"verified"`
+	CreatedAt       time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt       time.Time          `json:"updated_at" bson:"updated_at"`
+}
