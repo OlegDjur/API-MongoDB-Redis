646d5b63f0e884ee1f24bd16b70c87cd74b3c302
Oleg_Dzhur <olegdjur@gmail.com>
2023-02-15 13:23:59 +0600

    created function createToken

--- utils/token.go
+++ utils/token.go
@@ -34,3 +34,32 @@ func CreateToken(ttl time.Duration, payload interface{}, privateKey string) (str
 
 	return token, nil
 }
+
+func ValidateToke(token string, publicKey string) (interface{}, error) {
+	dacodePublicKey, err := base64.StdEncoding.DecodeString(publicKey)
+	if err != nil {
+		return nil, fmt.Errorf("cloud not decode: %w", err)
+	}
+
+	key, err := jwt.ParseEdPublicKeyFromPEM(dacodePublicKey)
+	if err != nil {
+		return "", fmt.Errorf("validate: parse key: %w", err)
+	}
+
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("unexpected method: %s", t.Header["alg"])
+		}
+		return key, nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("validate: %w", err)
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok || !parsedToken.Valid {
+		return nil, fmt.Errorf("validate: invalid token")
+	}
+
+	return claims["sub"], nil
+}
