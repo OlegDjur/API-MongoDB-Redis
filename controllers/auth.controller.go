dcc544310cacc796268c58b634f4705eeffab9df
Oleg_Dzhur <olegdjur@gmail.com>
2023-02-15 14:55:00 +0600

    created function SignInUser controller

--- controllers/auth.controller.go
+++ controllers/auth.controller.go
@@ -6,7 +6,10 @@ import (
 
 	"github.com/OlegDjur/API-MongoDB-Redis/models"
 	"github.com/OlegDjur/API-MongoDB-Redis/services"
+	"github.com/OlegDjur/API-MongoDB-Redis/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/go-delve/delve/pkg/config"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type AuthController struct {
@@ -42,4 +45,49 @@ func (ac *AuthController) SignUpUser(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, gin.H{"status": "success", "data": gin.H{"user": models.FilteredResponse(newUser)}})
+}
+
+func (ac *AuthController) SignInUser(ctx *gin.Context) {
+	var credentials *models.SignInInput
+
+	if err := ctx.ShouldBindJSON(&credentials); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	user, err := ac.userService.FindUserByEmail(credentials.Email)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": "Invalid email or password"})
+			return
+		}
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	if err := utils.VerifyPassword(user.Password, credentials.Password); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": "Invalid email or Password"})
+		return
+	}
+
+	config, _ := config.LoadConfig(".")
+
+	// Generate Tokens
+	access_token, err := utils.CreateToken(config.AccessTokenExpiresIn, user.ID, config.AccessTokenPrivateKey)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	refresh_token, err := utils.CreateToken(config.RefreshTokenExpiresIn, user.ID, config.AccessTokenPrivateKey)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	ctx.SetCookie("access_token", access_token, config, config.AccessTokenMaxAge*60, "/", "localhost", false, true)
+	ctx.SetCookie("refresh_token", refresh_token, config.RefreshTokenMaxAge*60, "/", "localhost", false, true)
+	ctx.SetCookie("logged_in", "true", config.AccessTokenMaxAge*60, "/", "localhost", false, false)
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "access_token": access_token})
 }
